715eadf83dc318fe799b8e4b48d9c73c9c95fb25
Nutto55 <pichasamon@gmail.com>
2023-06-16 02:44:03 +0700

    Add tts function

--- bot.py
+++ bot.py
@@ -6,11 +6,7 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-intents.guilds = True
-intents.voice_states = True
+intents = discord.Intents.all()
 
 bot = commands.Bot(intents=intents, command_prefix='!')
 
@@ -33,22 +29,22 @@ async def greeting(interaction: discord.Interaction):
         user_voice = interaction.user.voice
 
         if not user_voice:
-            await interaction.response.send_message("Sorry, you should be in voice channel.", ephemeral=True)
+            await interaction.response.send_message("Sorry, you should be in voice channel.")
             return
 
         bot_voice = interaction.guild.voice_client
 
         if bot_voice:
             if bot_voice.channel.id == user_voice.channel.id:
-                await interaction.response.send_message("I'm in your voice channel!", ephemeral=True)
+                await interaction.response.send_message("I'm in your voice channel!")
                 return
 
             await bot_voice.move_to(user_voice.channel)
-            await interaction.response.send_message("I'm moving to your channel", ephemeral=True)
+            await interaction.response.send_message("Hello!", tts=True)
             return
 
         await user_voice.channel.connect()
-        await interaction.response.send_message("Joined your channel :)", ephemeral=True)
+        await interaction.response.send_message("Hello!", tts=True)
     except Exception as e:
         print(e)
         await interaction.response.send_message('Joining failed')
